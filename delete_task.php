09f14da2d23fed62b040c06b8b2b4b0685c21854
Dayana M <dbmv23@gmail.com>
2021-06-04 12:48:15 -0500

    First Commit

---
+++ delete_task.php
@@ -0,0 +1,14 @@
+<?php include("db.php");
+
+    if(isset($_GET['id'])){
+        $id= $_GET['id'];
+            $query= "DELETE FROM task WHERE id = $id";
+            $result = mysqli_query($conn,$query);
+            if(!$result){
+                die("Query fail");
+            }
+            $_SESSION['message']='Task deleted successfully';
+            $_SESSION['message_type']='danger';
+            header("Location: index.php");
+    }
+?>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MYSQL CRUD</title>
+
+    <!-- Bootstrap 4-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    
+    <!-- Font awesome-->
+    <script src="https://kit.fontawesome.com/289dd869cf.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<nav class= "navbar navbar-dark bg-dark">
+<div class= "container">
+<a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+</div></nav>
